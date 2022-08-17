268c0d6188bb78f18d2c7d5da5840c89f02428fb
FuBaooo <baihe2233@gmail.com>
2022-08-17 16:52:41 +0800

    fix: 修改

--- pages/index.vue
+++ pages/index.vue
@@ -5,19 +5,24 @@ const { data } = await useFetch('/api/almanac')
 <template>
   <section>
     <Suspense>
-      <div m-auto inline-block text-left>
-        <div>
-          宜: {{ data.yi.map(i => `${i.name}${i.emoji}`).join('、') }}
+      <div>
+        <div mb-10 mt-10 text-lg>
+          自律人今天不懂吃啥？看看食材黄历吧！
         </div>
-        <div>
-          忌: {{ data.ji.map(i => `${i.name}${i.emoji}`).join('、') }}
-        </div>
-        <div h-1px bg-gray-300 />
-        <div>
-          宜: {{ data.yiStr }}
-        </div>
-        <div>
-          忌: {{ data.jiStr }}
+        <div m-auto inline-block text-left>
+          <div>
+            宜: {{ data.yi.map(i => `${i.name}${i.emoji}`).join('、') }}
+          </div>
+          <div>
+            忌: {{ data.ji.map(i => `${i.name}${i.emoji}`).join('、') }}
+          </div>
+          <div h-1px bg-gray-300 my-10 />
+          <div>
+            宜: {{ data.yiStr }}
+          </div>
+          <div>
+            忌: {{ data.jiStr }}
+          </div>
         </div>
       </div>
     </Suspense>
