1ad7287ad4a792ce210b450c4cc05e1d332b1e06
FuBaooo <baihe2233@gmail.com>
2022-08-25 10:00:40 +0800

    feat: new field api

--- server/api/almanac/str.ts
+++ server/api/almanac/str.ts
@@ -2,6 +2,7 @@ import axios from 'axios'
 import dayjs from 'dayjs'
 import type { Keyword } from '~~/data/match'
 import { match } from '~~/data/match'
+import everyday from '~~/data/everyday.json'
 
 export interface AlmanacItem {
   title: string
@@ -16,11 +17,13 @@ export default defineEventHandler(async () => {
   const { data } = await axios.get(url)
   const yi = match(data.html.yi).sort((a, b) => b.name.length - a.name.length).reverse()
   const ji = match(data.html.ji).sort((a, b) => b.name.length - a.name.length).reverse()
+  const e = everyday as string[]
 
   const result = yi.filter(item => !ji.some(jiItem => jiItem.name === item.name))
 
   return {
     yi: result.map(keyword => `${keyword.emoji} ${keyword.name}`).join('、'),
     ji: ji.map(keyword => `${keyword.emoji} ${keyword.name}`).join('、'),
+    everyday: e[Math.floor(Math.random() * e.length)],
   }
 })
