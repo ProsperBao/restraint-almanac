f21abf255d8f1eb9f7712f77b40d12807ac86e32
FuBaooo <baihe2233@gmail.com>
2022-08-26 15:32:15 +0800

    fix: 修改

--- server/api/almanac/picture.ts
+++ server/api/almanac/picture.ts
@@ -1,13 +1,17 @@
-import almanac from '~~/utils/almanac'
-import { DrawCard } from '~~/utils/draw'
+// import almanac from '~~/utils/almanac'
+// import { DrawCard } from '~~/utils/draw'
+import fs from 'fs'
 
 export default defineEventHandler(async () => {
-  const result = await almanac()
-  try {
-    const drawCard = new DrawCard({})
-    return drawCard.draw(result)
-  }
-  catch (e) {
-    return e
-  }
+  // const result = await almanac()
+  // try {
+  //   const drawCard = new DrawCard({})
+  //   return drawCard.draw(result)
+  // }
+  // catch (e) {
+  //   return e
+  // }
+  // 输出当前目录下所有文件
+  const files = fs.readdirSync('./')
+  return files
 })
