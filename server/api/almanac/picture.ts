f02542fb6bb5eaeea8ad4e096fa6e77fa31a90dc
FuBaooo <baihe2233@gmail.com>
2022-08-28 14:13:51 +0800

    fix: build

--- server/api/almanac/picture.ts
+++ server/api/almanac/picture.ts
@@ -1,36 +1,11 @@
-// import almanac from '~~/utils/almanac'
+import almanac from '~~/utils/almanac'
 // import { DrawCard } from '~~/utils/draw'
-import { fabric } from 'fabric'
 
 export default defineEventHandler(async () => {
-  // const result = await almanac()
+  const result = await almanac()
 
   try {
-    const canvas = new fabric.Canvas('canvas')
-    const rect = new fabric.Rect({
-      width: 50,
-      height: 50,
-      left: 100,
-      top: 100,
-      stroke: '#aaf',
-      strokeWidth: 1,
-      fill: '#faa',
-      selectable: false,
-    })
-    canvas.add(rect)
-    const rect1 = new fabric.Rect({
-      width: 50,
-      height: 50,
-      left: 50,
-      top: 50,
-      stroke: '#faa',
-      strokeWidth: 1,
-      fill: '#aaf',
-      selectable: false,
-    })
-    canvas.add(rect)
-    canvas.add(rect1)
-    return canvas.toSVG()
+    return result
   }
   catch (e) {
     return e
