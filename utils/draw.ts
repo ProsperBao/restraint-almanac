61760b2c348a4ddf8fa19b87bdb761c7c26eba9b
FuBaooo <baihe2233@gmail.com>
2022-08-28 13:29:21 +0800

    feat: test code

--- utils/draw.ts
+++ utils/draw.ts
@@ -106,6 +106,18 @@ export class DrawCard {
       selectable: false,
     })
     canvas.add(rect)
+    const rect1 = new fabric.Rect({
+      width: 50,
+      height: 50,
+      left: 50,
+      top: 50,
+      stroke: '#faa',
+      strokeWidth: 1,
+      fill: '#aaf',
+      selectable: false,
+    })
+    canvas.add(rect)
+    canvas.add(rect1)
     return canvas.toSVG()
   }
 
