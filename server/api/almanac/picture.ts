645a29abdb679d019caabc2b3627b03bdd9322c7
FuBaooo <baihe2233@gmail.com>
2022-08-29 09:08:52 +0800

    fix: content type

--- server/api/almanac/picture.ts
+++ server/api/almanac/picture.ts
@@ -1,8 +1,10 @@
 import almanac from '~~/utils/almanac'
 import { DrawCard } from '~~/utils/draw'
 
-export default defineEventHandler(async () => {
+export default defineEventHandler(async (ctx) => {
   const result = await almanac()
+
+  ctx.res.setHeader('Content-Type', 'image/svg+xml')
 
   try {
     const drawCard = new DrawCard({})
