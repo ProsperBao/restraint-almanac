f23a5652375d8b33141d30d917be43570f40b6af
FuBaooo <baihe2233@gmail.com>
2022-08-29 15:06:24 +0800

    feat: svg complete

--- utils/draw.ts
+++ utils/draw.ts
@@ -1,5 +1,4 @@
 import jsdom from 'jsdom'
-import { omit } from 'lodash'
 import type { AlmanacResult } from './almanac'
 
 export interface DrawCardOptions {
@@ -59,15 +58,18 @@ export class DrawCard {
     return use
   }
 
+  /**
+   * calculate all element location
+   */
   calcAllPosition(data: AlmanacResult): DrawCardHalfDistrict {
     const { width, startX, startY, fontSize, padding, fontPadding } = this.options
     const halfDistrict: DrawCardHalfDistrict = [[], []]
 
-    let [countWith, countHeight] = [startX, startY]
+    let [countWith, countHeight] = [startX + padding, startY]
     for (let i = 0; i < data.length; i++) {
       if (i === 1) {
-        countWith = startX
-        countHeight += padding * 6
+        countWith = startX + padding
+        countHeight += padding * 4
       }
       data[i].list.forEach((item) => {
         const text = `${item.emoji} ${item.name}`
@@ -77,7 +79,7 @@ export class DrawCard {
         const rectHeight = fontSize + fontPadding * 1.5
 
         if (rectWidth > width - countWith - padding) {
-          countWith = startX
+          countWith = startX + padding
           countHeight += rectHeight + padding
         }
 
@@ -98,7 +100,7 @@ export class DrawCard {
   }
 
   draw(data: AlmanacResult) {
-    const { width, margin, padding } = this.options
+    const { width, margin, padding, startX, fontSize } = this.options
     const halfDistrict = this.calcAllPosition(data)
     const last = halfDistrict[1][halfDistrict[1].length - 1]
     const height = last.y + last.height + (padding / 2)
@@ -113,14 +115,31 @@ export class DrawCard {
       height,
     })
 
-    for (let i = 0; i < halfDistrict.length; i++)
+    for (let i = 0; i < halfDistrict.length; i++) {
+      if (i === 1) {
+        const y = halfDistrict[i][0].y - padding
+        this.createLine(margin, y, width + margin, y)
+      }
+
       halfDistrict[i].forEach(item => this.createTextRect(item))
+
+      const districtLast = halfDistrict[i][halfDistrict[i].length - 1]
+      const districtFirst = halfDistrict[i][0]
+      this.createText(
+        startX / 2 - (fontSize / 2),
+        districtFirst.y + (((districtLast.y + districtLast.height + padding) - districtFirst.y) / 2),
+        fontSize * 1.25,
+        data[i].title,
+      )
+    }
+
+    this.createLine(startX, margin, startX, height + margin)
 
     return this.svg?.outerHTML
   }
 
   /**
-   * 创建SVG元素
+   * create svg elements
    */
   createSvgElement<T extends keyof SVGElementTagNameMap>(element: T, attributes: Record<string, string | number> | string = '') {
     const ele = this.doc.createElementNS<T>('http://www.w3.org/2000/svg', element)
@@ -138,44 +157,75 @@ export class DrawCard {
     return ele
   }
 
+  /**
+   * create rounded rectangular
+   */
   createBorderRect({ x, y, height, width }: Omit<DrawCardTagRect, 'length' | 'content'>, g: SVGGElement | null = null) {
     const { radius } = this.options
     const target = g || this.svg
     target?.appendChild(this.createSvgElement('rect', {
-      'fill': '#fff',
-      'stroke': '#e5e7eb',
-      'rx': radius,
-      'ry': radius,
+      fill: '#ffffff',
+      stroke: '#e5e7eb',
+      rx: radius,
+      ry: radius,
       x,
       y,
       width,
       height,
-      'stroke-width': 1,
     }))
   }
 
   /**
-   * 包含文字的圆角矩形
+   * rounded rectangular containing text
    */
   createTextRect({ x, y, height, width, content, length }: DrawCardTagRect) {
     const { fontSize } = this.options
     const g = this.createSvgElement('g')
     this.createBorderRect({ x, y, height, width }, g)
 
-    const text = this.createSvgElement('text', {
-      'fill': '#000',
-      'fontSize': fontSize,
-      'x': x + (width / 2) - length / 2,
-      'y': y + height - (fontSize / 2),
-      'baseline-shift': 'baseline',
-    })
-    text.textContent = content
-    g.appendChild(text)
+    this.createText(
+      x + (width / 2) - length / 2,
+      y + height - (fontSize / 2),
+      fontSize,
+      content,
+      g,
+    )
     this.svg?.appendChild(g)
   }
 
   /**
-   * 计算包含 emoji 的文本长度
+   * create a line
+   */
+  createLine(x1: number, y1: number, x2: number, y2: number) {
+    const line = this.createSvgElement('line', {
+      x1,
+      y1,
+      x2,
+      y2,
+      'stroke': '#e5e7eb',
+      'stroke-width': 1,
+    })
+    this.svg?.appendChild(line)
+  }
+
+  /**
+   * create text
+   */
+  createText(x: number, y: number, fontSize: number, content: string, g: SVGGElement | null = null) {
+    const text = this.createSvgElement('text', {
+      'fill': '#000',
+      'font-size': fontSize,
+      'x': x,
+      'y': y,
+      'baseline-shift': 'baseline',
+    })
+    text.textContent = content
+    const target = g || this.svg
+    target?.appendChild(text)
+  }
+
+  /**
+   * calculate the text length containing emoji
    */
   calcEmojiTextLength(text: string) {
     const { fontSize } = this.options
@@ -185,119 +235,4 @@ export class DrawCard {
     else
       return 0
   }
-
-  // draw(data: AlmanacResult) {
-  //   const { width, margin, startX, border, padding } = this.options
-
-  //   const halfDistrict = this.calcAllPosition(data)
-  //   const last = halfDistrict[1][halfDistrict[1].length - 1]
-  //   const height = last.y + last.height + (padding / 2) + border
-
-  //   const canvas = createCanvas(width + margin * 2, height + margin * 2)
-
-  //   registerFont('/segoeui.ttf', { family: 'SegoeUI' })
-  //   this.ctx = canvas.getContext('2d')!
-
-  //   if (this.ctx)
-  //     this.ctx.fillStyle = 'rgba(255, 255, 255, 0)'
-
-  //   // 绘制背景边框
-  //   this.drawRoundRect({ x: margin, y: margin }, width, height, 4)
-  //   for (let i = 0; i < halfDistrict.length; i++) {
-  //     if (i === 1)
-  //       this.drawLine({ x: margin, y: halfDistrict[i][0].y - (padding * 0.8) }, { x: width + margin, y: halfDistrict[i][0].y - (padding * 0.8) })
-
-  //     halfDistrict[i].forEach((item) => {
-  //       this.drawTextRoundRect(item, item.content)
-  //     })
-
-  //     const districtLast = halfDistrict[i][halfDistrict[i].length - 1]
-  //     const districtFirst = halfDistrict[i][0]
-  //     this.drawText({
-  //       x: startX / 2,
-  //       y: districtFirst.y + (((districtLast.y + districtLast.height + padding + margin) - districtFirst.y) / 2),
-  //     }, data[i].title)
-  //   }
-  //   this.drawLine({ x: startX, y: margin }, { x: startX, y: height + margin + border })
-
-  //   return canvas.toBuffer('image/png', { compressionLevel: 3, filters: canvas.PNG_FILTER_NONE })
-  // }
-
-  // drawRoundRectPath(width: number, height: number, radius: number) {
-  //   if (!this.ctx)
-  //     return
-  //   this.ctx.beginPath()
-  //   this.ctx.arc(width - radius, height - radius, radius, 0, Math.PI / 2)
-  //   this.ctx.lineTo(radius, height)
-  //   this.ctx.arc(radius, height - radius, radius, Math.PI / 2, Math.PI)
-  //   this.ctx.lineTo(0, radius)
-  //   this.ctx.arc(radius, radius, radius, Math.PI, Math.PI * 3 / 2)
-  //   this.ctx.lineTo(width - radius, 0)
-  //   this.ctx.arc(width - radius, radius, radius, Math.PI * 3 / 2, Math.PI * 2)
-  //   this.ctx.lineTo(width, height - radius)
-  //   this.ctx.closePath()
-  // }
-
-  // drawRoundRect(position: DrawCardPosition, width: number, height: number, radius: number) {
-  //   if (!this.ctx)
-  //     return
-  //   if (2 * radius > width || 2 * radius > height)
-  //     return false
-  //   this.ctx.save()
-  //   this.ctx.translate(position.x, position.y)
-  //   this.drawRoundRectPath(width, height, radius)
-
-  //   this.ctx.lineWidth = 1
-  //   this.ctx.strokeStyle = '#e5e7eb'
-  //   this.ctx.stroke()
-  //   this.ctx.shadowOffsetX = 2
-  //   this.ctx.shadowOffsetY = 2
-  //   this.ctx.shadowColor = 'rgba(0,0,0,0.1)'
-  //   this.ctx.shadowBlur = 4
-  //   this.ctx.fillStyle = '#ffffff'
-  //   this.ctx.fill()
-
-  //   this.ctx.restore()
-  // }
-
-  // drawTextRoundRect(tagRect: DrawCardTagRect, content: string) {
-  //   if (!this.ctx)
-  //     return
-  //   const { fontSize, padding, font } = this.options
-  //   const position = { x: tagRect.x, y: tagRect.y }
-  //   this.drawRoundRect(position, tagRect.width + (padding / 2), tagRect.height, 4)
-  //   // 填充文字
-  //   this.ctx.font = `${fontSize}px ${font}`
-  //   this.ctx.textAlign = 'center'
-  //   this.ctx.textBaseline = 'middle'
-  //   this.ctx.fillStyle = '#000000'
-  //   this.ctx.fillText(content, position.x + tagRect.width / 2 + (padding / 4), position.y + tagRect.height / 2)
-  // }
-
-  // drawLine(start: DrawCardPosition, end: DrawCardPosition) {
-  //   if (!this.ctx)
-  //     return
-  //   this.ctx.beginPath()
-  //   this.ctx.moveTo(start.x, start.y)
-  //   this.ctx.lineTo(end.x, end.y)
-  //   this.ctx.strokeStyle = '#e5e7eb'
-  //   this.ctx.lineWidth = 1
-  //   this.ctx.stroke()
-  //   this.ctx.closePath()
-  // }
-
-  // drawText(position: DrawCardPosition, content: string) {
-  //   if (!this.ctx)
-  //     return
-  //   const { fontSize, font } = this.options
-  //   const size = fontSize * 1.25
-  //   this.ctx.beginPath()
-  //   this.ctx.moveTo(position.x, position.y)
-  //   this.ctx.font = `${size}px ${font}`
-  //   this.ctx.textAlign = 'center'
-  //   this.ctx.textBaseline = 'middle'
-  //   this.ctx.fillStyle = '#000000'
-  //   this.ctx.fillText(content, position.x + (size / 4), position.y - (size / 2))
-  //   this.ctx.closePath()
-  // }
 }
